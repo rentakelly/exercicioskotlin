4635cd22fe2c92b918428f705aef7a4324c0b9e1
rentakelly <rentakelly@hotmail.com>
2023-01-05 15:46:45 -0300

    Adiciona primeiro commit com primeira exercicio

---
+++ hello.kt
@@ -0,0 +1,17 @@
+package com.example.myexercices
+
+fun main() {
+    println("Hello, Kotlin Apprentice reader!")
+    println(2 + 6)
+    println(10 - 2)
+    println(2 * 4)
+    println(24 / 3)
+    println( 28 % 10)
+    val banana = ( 28.0 % 10.0)
+    println("%.0f".format(banana))
+    println(1 shl 3)
+    println(32 shl 2)
+    println(((8000 / (5 * 10)) - 32) shr (29 % 5))
+    println( 350 / 5 + 2)
+    println( 350 / (5 + 2))
+}
